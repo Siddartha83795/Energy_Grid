f8d0379248ff6655bca875bff9f473311bb5a403
Siddartha83795 <alonrfox3797@gmail.com>
2026-06-13 19:37:03 +0530

    feat: add vercel.json, render.yaml, install cross-env and integrate nitro plugin for deployments

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
+import { nitro } from "nitro/vite";
 
 export default defineConfig({
   server: {
@@ -14,6 +15,7 @@ export default defineConfig({
     tanstackStart({
       server: { entry: "server" },
     }),
+    nitro(),
     react(),
     tailwindcss(),
   ],
